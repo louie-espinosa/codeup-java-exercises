09b5304707299b20173ea8ee707833cfda423b2f
Louie Espinosa <louie.espinosa@proton.me>
2022-08-03 14:39:01 -0500

    Completed exercise 1 String Basics

---
+++ src/StringExercise.java
@@ -0,0 +1,13 @@
+public class StringExercise {
+    public static void main(String[] args) {
+     String pinkFloyd =   "We don't need no education; We don't need no thought control";
+     String s = "Check 'this' out!, 's inside of 's!";
+     String wDrive = "In windows, the main drive is usually C:\\";
+     String slasher = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!"; //escape each backslash.
+
+        System.out.println(pinkFloyd + "\n" + s + "\n" + wDrive +"\n" + slasher);
+
+        //ex 2.
+
+    }
+}
