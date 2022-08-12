872464e181f7106659ef47288022c544622a24fd
Louie Espinosa <louie.espinosa@proton.me>
2022-08-12 16:19:37 -0500

    Added variable for random noun and completed ServerGenerator exercise

--- src/ServerNameGenerator/ServerNameGenerator.java
+++ src/ServerNameGenerator/ServerNameGenerator.java
@@ -4,14 +4,9 @@ import java.util.Random;
 
 public class ServerNameGenerator {
     private static Random random = new Random();
-    public static String [] adjectives = {"sultry", "stubby", "surly"};
-    public static String [] nouns = {"lung", "cello"};
+    public static String [] adjectives = {"sultry", "stubby", "surly", "thicc", "athletic"};
+    public static String [] nouns = {"raspberry", "blueberry", "redcurrant", "boysenberry", "cranberry"};
 
-    //run it here
-    public static void main(String[] args) {
-        String randomAdjective = getRandomStringFromArr(adjectives);
-        System.out.println(randomAdjective);
-    }
 
     public static String getRandomStringFromArr(String[] strings) {
         //1. generate a random index from 0 to last element in strings
@@ -19,5 +14,13 @@ public class ServerNameGenerator {
         int rando = random.nextInt(strings.length);
         //2. return the element at the random index in strings
         return strings[rando];
+    }
+
+    public static void main(String[] args) {
+        String randomAdjective = getRandomStringFromArr(adjectives);
+        System.out.println("Your random adjective is: " + randomAdjective);
+
+        String randomNoun = getRandomStringFromArr(nouns);
+        System.out.println("Your random noun is: " + randomNoun);
     }
 }
