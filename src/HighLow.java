65f2cbbd3a6bea50f4202adaae2fc93ef27788c6
Louie Espinosa <louie.espinosa@proton.me>
2022-08-19 13:46:45 -0500

    Testing Java spliterator() interface

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,12 +1,41 @@
+import java.util.*;
+
 public class HighLow {
 
 
     public static void main(String[] args) {
 
+                // create an ArrayList which contains
+                // emails for a group of people
+                ArrayList<String> list = new ArrayList<String>();
+
+                // Add emails to list
+                list.add("abc@geeksforgeeks.org");
+                list.add("user@geeksforgeeks.org");
+                list.add("pqr@geeksforgeeks.org");
+                list.add("random@geeksforgeeks.org");
+                list.add("randomuser@geeksforgeeks.org");
+
+                // create Spliterator of ArrayList
+                // using spliterator() method
+                Spliterator<String> emails = list.spliterator();
+
+                // print result from Spliterator
+                System.out.println("list of Emails:");
+
+                // forEachRemaining method of Spliterator
+                emails.forEachRemaining((n) -> System.out.println(n));
+
+
+
+
+
+
     } //ex 5.
 //    You are going to build a high-low guessing game. Create a class named HighLow inside of src.
 //
 //    The specs for the game are:
+    //Math
 //    Game picks a random number between 1 and 100.
 //    Prompts user to guess the number.
 //    All user inputs are validated.
